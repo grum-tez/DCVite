85baf9a7952d28f34a18e2bdb36a1b41a10eed61
grum-tez <grum.tez@gmail.com>
2023-07-21 08:04:09 +0000

    simplify onCreate.sh

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -1,6 +1,3 @@
 yarn global add chokidar-cli \
-&& npm i @completium/completium-cli -g \
-&& completium-cli init \
-&& cd blockchain \
-&& completium-cli mockup init \
+&& yarn global add @completium/completium-cli \
 && yarn install
